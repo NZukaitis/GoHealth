6c58058921b7d524e021f45b743f49ac5b1560b6
Nick Zukaitis <nzukaitis@albany.edu>
2022-10-23 20:52:58 -0400

    got rid of vunerability

--- backend/indexB.js
+++ backend/indexB.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host:"saluswell-db.c7m1a1ppk302.us-east-1.rds.amazonaws.com",
-    port:"3306",
-    user:"saluswell_admin",
-    password:"Cyglera!101", //generally a horrible practice, just being used to make sure everything works
-    database:"saluswell_dev"
+    host:"",
+    port:"",
+    user:"",
+    password:"", //generally a horrible practice, just being used to make sure everything works
+    database:""
 });
 
 db.connect((err) => {
