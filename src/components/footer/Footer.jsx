115a14d97e769585e2533c4df2d0c5310a6bc2f1
a1shamraj <63331919+a1shamraj@users.noreply.github.com>
2022-10-24 23:57:03 -0400

    footer didnt work idk

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import './footer.css';
-import logo from './logo.svg'
+// import logo from 'logo.svg'
 
 function Footer() {
     return (
     <footer className="footer">
-        <img src={logo} alt="Logo"/>;
+        {/* <img src={logo} alt="Logo"/>; */}
     </footer>
     );
 }
