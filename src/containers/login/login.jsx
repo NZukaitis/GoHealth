f496ca0f5f8724feb38cac349b7e78e5045f9025
Sam Schaeffler <93324016+probablySmam@users.noreply.github.com>
2022-11-07 11:48:04 -0500

    fixed colors and login page

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -7,23 +7,25 @@ function Login() {
                 GoHealthi
             </h1>
 
-            <button class="signup" onclick="document.location='signup.html'"> Sign up </button>
+            <div class="signupLogin">
+                <button class="signup" onclick="document.location='signup.html'"> Sign up </button>
 
-            <main class = "login">
-                <h2>Login</h2>
+                <div class = "login section">
+                    <h2>Login</h2>
 
-                <form>
-                    <label for="username">Username:</label><br></br>
-                    <input type="text" id="username"></input><br></br><br></br>
-                    <label for="password">Password:</label><br></br>
-                    <input type="text" id="password"></input><br></br><br></br>
-                    <input type="submit" value="Log in"></input>
-                </form>
+                    <form>
+                        <label for="username">Username:</label><br></br>
+                        <input type="text" id="username"></input><br></br><br></br>
+                        <label for="password">Password:</label><br></br>
+                        <input type="text" id="password"></input><br></br><br></br>
+                        <input type="submit" value="Log in"></input>
+                    </form>
 
-                <br></br>
-                <a class="forgot" href="#forgot"> forgot password</a>
+                    <br></br>
+                    <a class="forgot" href="#forgot"> forgot password</a>
 
-            </main>
+                </div>
+            </div>
         </div>
     );
   }
